3f965faa755ee1a80929813022d90d4c237181ea
Jiaqi <dxq7nz@virginia.edu>
2023-01-26 00:22:41 -0500

    try

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,8 @@
-const express = require("express");
+import express from "express";
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+router.get("/", async (req, res) => {
+  return res.send("Hello From Express!");
 });
 
-module.exports = router;
+export default router;
